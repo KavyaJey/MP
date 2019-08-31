c15f85c7afcaea85a6e93130983fce8769c58e3a
KavyaJey <kavya.jey@gmail.com>
2019-08-31 15:16:57 +0530

    instruction changes

--- index.php
+++ index.php
@@ -14,14 +14,21 @@
 		?>
 		<header>
 			<h1 id="title">MP Scheduler</h1>
-			<p id="about">This is a MP mock scheduler. This makes it much easier to make schedules when you play Math Pentathlon. You can make a schedule in 3 easy steps: </p>
-			<h2 id="steps">Add Students > Add Monitors > Select Division > Create Schedule</h2>
+      <h3>Instructions</h3>
+      <p>
+        This is a MP mock scheduler. Generating a mock tournament schedule using this schedule maker is very simple. Just follow the steps below: </br>
+
+        1. Enter the first & last names of the players who will be playing in the tournament and make sure to include a comma after the child's last name. 
+        2. Enter the first & last names of the monitor who have volunteered to be in the mock tournament and make sure to include a comma after the monitor's last name. 
+        3. Choose the division that the students are playing in. 
+        4. Click the 'Submit' button, and voilà! Your tournament schedule is ready.
+      </p>
 		</header>
 		<main>
       <form action="schedulePageNew.php" method="post" id="form">
-			<h3>Add Students: <span class="definition">List the students</span><h5>Please put commas between the names.</h5></h3>
+			<h3>Add Students: <span class="definition">Enter the first & last names of the students</span><h5>Make sure to include a comma after the child's last name.</h5></h3>
       <textarea id="studentInputs" name="students"></textarea>
-			<h3>Add Monitors: <span class="definition">List the monitors</span><h5>Please put commas between the names.</h5></h3>
+			<h3>Add Monitors: <span class="definition">Enter the first & last names of the monitors</span><h5>Make sure to include a comma after the child's last name</h5></h3>
 			<textarea id="monitorInputs" name="monitors"></textarea>
 			<h3>Division: <span class="definition">Select the division the students & monitors are in</span></h3>
       <select id="dropdown" name="division">
@@ -41,18 +48,6 @@
     	I created this website, because I wanted to fix a problem. Scheduling <a href="https://www.mathpentath.org" target="_blank">Math Pentathlon</a> mock tournaments were extremely time-consuming and took a lot of brain-power. I wanted to make it easier. Hopefully this is useful to some of you!
 
 		This website uses round-robin scheduling. A word of thanks goes out to <a href="https://nrich.maths.org/1443" target="_blank"> NRICH's excellent website</a> that helped me understand the algorithms required for this.
-    </p>
-
-    <h3>
-    	Instructions
-    </h3>
-    <p>
-    	Generating a mock tournament schedule using this schedule maker is very simple. Just follow the steps below: </br>
-
-    	1. Enter the names of the players who will be playing in the tournament. </br>
-    	2. Enter the names of the monitor who have volunteered to be in the tournament. </br>
-    	3. Choose the division that the students are playing in. </br>
-    	4. Click the 'Submit' button, and voilà! Your tournament schedule is ready.
     </p>
       
 	<!--comment test-->
